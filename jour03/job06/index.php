92c0d157ef2642be5b5b8dd0b9ca7caa4a6f6660
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-16 14:50:27 +0200

    job 06 exo reecrire une chaine a lenver

---
+++ jour03/job06/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Créez une variable de type string nommée $str et affectez y le texte :
+// “Les choses que l'on possède finissent par nous posséder."
+// Créez un algorithme qui parcourt et écrit cette chaine à l’envers.
+// Ex. : redessop suon rap tnessinif edessop no'l euq sesohc seL
+$str = "Les choses que l'on possede finissent par nous posseder.";
+$reversedStr = "";
+for ($i = strlen($str) - 1; $i >= 0; $i--) {
+    $reversedStr .= $str[$i];
+}
+echo $reversedStr;
