f4663ea03f9a91d0b1bbe5287e01d4d1bd838800
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-16 14:03:23 +0200

    job02 exo pour afficher un caractere sur 2 dune chaine caractere

---
+++ jour03/job02/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Créez une variable de type string nommée $str et affectez y le texte :
+// “Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.”
+// Parcourir cette chaîne en affichant seulement un caractère sur deux.
+// Ex. : Tu e ntnssrn edsdn etmscmelslre osl li.
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+for ($i = 0; $i < strlen($str); $i += 2) {
+    echo $str[$i];
+}
