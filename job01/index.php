1da63821090560dd015c4686859531e382cb64ec
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-16 13:38:14 +0200

    initialisation projet Jour03 avec ajout exo job01

---
+++ job01/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// Créez un tableau contenant les nombres 200, 204, 173, 98, 171, 404, 459.
+// Parcourez ce tableau et affichez pour chaque nombre “X est paire<br />” ou “X est
+// impaire<br />”, sur votre page index.php. X prenant tour à tour chacune des valeurs
+// comprises dans ce tableau.
+// Ex. : 200 est paire
+// 204 est paire
+// 173 est impaire
+// 98 est paire
+// 171 est impaire
+// 404 est paire
+// 459 est impaire
+$tab = [200, 204, 173, 98, 171, 404, 459];
+foreach ($tab as $value) {
+    if ($value % 2 == 0) {
+        echo "$value est paire<br />";
+    } else {
+        echo "$value est impaire<br />";
+    }
+}
