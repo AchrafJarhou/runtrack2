ad8e5699f02b8eff062290bfd5efa0480db09fe2
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-17 14:05:03 +0200

    version avec deux boucles pour afficher un triangle

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -3,6 +3,12 @@
 // Faire un algorithme qui affiche un triangle de 5 de hauteur. Cette dimension devra être
 // stockée dans une variable $hauteur, modifiable facilement.
 $hauteur = 5;
+// for ($i = 1; $i <= $hauteur; $i++) {
+//     echo str_repeat("*", $i) . "<br />";
+// }
 for ($i = 1; $i <= $hauteur; $i++) {
-    echo str_repeat("*", $i) . "<br />";
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "<br />";
 }
