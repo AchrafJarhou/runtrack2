be163143a680a1f2400ae5f6a807b7a555eb585a
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-17 14:19:24 +0200

    exo variable

---
+++ jour01/job0.9/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// affiche “Hello LaPlateforme!” en php en utilisant
+// echo. Ouvrez le avec votre navigateur préféré.
+
+echo "Hello LaPlateforme!";
---
+++ jour01/job01/index.php
@@ -0,0 +1,24 @@
+<?php 
+// Dans le fichier runtrack2/jour01/job01, créer un fichier index.php.
+// Dans ce fichier, créez une variable nommée str. Affectez lui la valeur
+// “LaPlateforme”. Affichez le contenu de la variable str. Créez une variable str2
+// contenant “Vive” et une variable str3 contenant “!”.
+// Affichez “Vive LaPlateforme !” en concaténant l’ensemble de ces variables.
+// ● Créez une variable nommée val. Affectez à cette variable la valeur 6.
+// Affichez le contenu de la variable. Ajoutez 4 à cette variable. Affichez à nouveau
+// le contenu.
+// ● Créez une variable myBool, affectez à cette variable true, affichez le contenu de
+// la variable. Affectez false à la variable et affichez à nouveau le contenu
+$str = "LaPlateforme";
+echo $str . "<br />";
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3 . "<br />";
+$val = 6;
+echo $val . "<br />";
+$val += 4;
+echo $val . "<br />";
+$myBool = true;
+echo $myBool . "<br />";
+$myBool = false;
+echo $myBool . "<br />";
