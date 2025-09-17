b642fdc3cfb89a2b7f004cfd747e4405fbdc278e
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-17 13:52:43 +0200

    algorithme qui affiche un rectangle

---
+++ jour02/job06/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Faire un algorithme qui affiche un rectangle de 20 de largeur par 10 de hauteur. Ces
+// dimensions devront être stockées dans deux variables $largeur et $hauteur, modifiables
+// facilement.
+$largeur = 20;
+$hauteur = 10;
+for ($i = 0; $i < $hauteur; $i++) {
+    for ($j = 0 ; $j < $largeur; $j++) {
+        echo "*";
+    }
+    echo "<br />";
+}
