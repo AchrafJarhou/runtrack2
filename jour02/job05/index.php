0e17c4b54dcbee6bd728bb602607a57e8ffd566e
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-17 13:40:40 +0200

    algorithme qui affiche les nombres premiers jusqu’à 1000

---
+++ jour02/job05/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Faire un algorithme qui affiche les nombres premiers jusqu’à 1000 en mettant un retour
+// à la ligne entre chaque nombre (“<br />”).
+
+for ($i = 2; $i <= 1000; $i++) {
+    $isPrime = true;
+    for ($j = 2; $j <= sqrt($i); $j++) {
+        if ($i % $j == 0) {
+            $isPrime = false;
+            break;
+        }
+    }
+    if ($isPrime) {
+        echo "$i<br />";
+    }
+}
