9831cdbca81e2300eeaf1050cbd0e5504e6a1db6
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-18 09:27:50 +0200

    une fonction qui affiche hello la plateforme

---
+++ jour07/job01/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// Créez une fonction nommée “hello()”. Cette fonction, une fois appelée, doit afficher sur
+// la page : “Hello LaPlateforme!”.
+function hello()
+{
+    echo "Hello LaPlateforme!";
+}
+hello();
